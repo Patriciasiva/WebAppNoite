8c485b1fc0ce960ddc9f58c5c92071186908d8f3
2024.4.12 <2024.4.12@DESKTOP-GN0OUCS>
2025-06-02 19:54:57 -0400

    Add project files.

---
+++ WebAppNoite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppNoite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
